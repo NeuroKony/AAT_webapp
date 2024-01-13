acd69e3b244e39afb07dc8176e144546d78f53ee
Wyhld <87336071+Wyhld@users.noreply.github.com>
2024-01-13 20:17:20 -0300

    Update Consentimiento.js

--- src/components/Consentimiento/Consentimiento.js
+++ src/components/Consentimiento/Consentimiento.js
@@ -31,15 +31,15 @@ const Consentimiento = () => {
                 <p>Dra. Constanza Baquedano</p>
                 <p>Escuela de Psicología, Universidad Adolfo Ibáñez</p>
                 <p>
-                  Usted ha sido invitado a participar en el estudio “Entendiendo la percepción de imágenes de comida” a cargo de la investigadora Post-Doctoral Dra. Constanza. Esta investigación se llevará a cabo entre los años 2024 - 2025, el objeto de esta carta es ayudarlo a tomar la decisión de participar en la presente investigación.
+                  Usted ha sido invitado a participar en el estudio “Entendiendo la percepción de imágenes de comida en el contexto de una aplicación para smartphone”, este estudio es dirigido por la Dra. Constanza Baquedano profesora asistente de la Escuela de Psicología de la Universidad Adolfo Ibáñez (UAI) y es financiado a través de fondos de Fondecyt posdoctoral y Varela Award entregados a Constanza Baquedano. El estudio ha sido aprobado por el comité de Ética de la Investigación de la Universidad Adolfo Ibáñez. Esta investigación se llevará a cabo entre los años 2024 - 2025 y el objeto de esta carta es ayudarlo a tomar la decisión de participar en la presente investigación.
                 </p>
                 <h2>¿Cuál es el propósito de esta investigación?</h2>
                 <p>
-                  El propósito de esta investigación es entender aspectos conductuales de la percepción de las imágenes de comida, y su función en la conducta alimentaria.
+                  El propósito de esta investigación es entender las emociones y reacciones que generan las imágenes de comida en el contexto de una aplicación para smartphone en un ambiente cotidiano.
                 </p>
                 <h2>¿En qué consiste su participación?</h2>
                 <p>
-                  Si acepta, su participación consistirá en realizar una tarea de reconocimiento visual, en las que verá imágenes de diferentes ítems de comida en la pantalla de su teléfono. Su tarea consistirá en alejarse o acercarse estas imágenes de acuerdo al color del marco con que estas imágenes sean presentadas. Al inicio y al final de la sesión se le pedirá llenar algunos cuestionarios con el objeto de conocer información básica sobre su estado antes y después de realizar las tareas.
+                  Si acepta, su participación consistirá en realizar una tarea de reconocimiento visual, en las que verá imágenes de diferentes ítems de comida en la pantalla de su teléfono. Su tarea consistirá en alejarse o acercarse estas imágenes de acuerdo al color del marco con que estas imágenes sean presentadas. Al inicio y al final de la sesión se le pedirá llenar algunos cuestionarios con el objeto de conocer información básica sobre su estado antes y después de realizar esta tarea, además de datos de información demográfica básica.
                 </p>
                 <h2>¿Cuánto durará su participación?</h2>
                 <p>
@@ -47,15 +47,15 @@ const Consentimiento = () => {
                 </p>
                 <h2>¿Qué riesgos corre al participar?</h2>
                 <p>
-                  Las metodologías utilizadas en este estudio, es decir el llenado de cuestionarios y la tarea conductual con imágenes de comida, no representan riesgo alguno para usted. 
+                  Las metodologías utilizadas en este estudio no representan riesgo alguno para usted, todas las metodologías utilizadas han sido extensamente validadas, por lo que se ocupan habitualmente en investigaciones de neurociencias cognitivas. 
                 </p>
                 <h2>¿Qué beneficios puede tener su participación?</h2>
                 <p>
-                  Este estudio no tiene beneficios directos para usted; sin embargo al participar estará contribuyendo al conocimiento científico sobre la percepción visual de imágenes de comida y sus posibles aplicaciones en la salud mental y física. 
+                  Este estudio no tiene beneficios directos para usted; sin embargo, al participar estará contribuyendo al conocimiento científico sobre la percepción visual de imágenes de comida en aplicaciones smartphone y sus posibles aplicaciones en la salud mental y física. 
                 </p>
                 <h2>¿Qué pasa con la información y datos que usted entregue?</h2>
                 <p>
-                  Los investigadores mantendrán CONFIDENCIALIDAD ABSOLUTA con respecto a cualquier información obtenida en este estudio y ninguna persona ajena a la investigación tendrá acceso alguno a los datos obtenidos. Los registros obtenidos durante la investigación serán codificados con un sistema alfanumérico, impidiendo así su identificación. Los datos aquí obtenidos podrían ser utilizados en otros análisis si usted lo autoriza (ver al final del consentimiento), siempre bajo el compromiso de total confidencialidad. Bajo ninguna circunstancia publicaremos ningún dato obtenido bajo alguna forma que permita identificarlo y si son presentados en el marco de una publicación científica, toda información relativa a su identidad será eliminada.
+                  Los investigadores mantendrán CONFIDENCIALIDAD ABSOLUTA con respecto a cualquier información obtenida en este estudio y ninguna persona ajena a la investigación tendrá acceso alguno a los datos obtenidos. La información obtenida durante esta investigación será codificada con un sistema alfanumérico, impidiendo así su identificación. Los datos aquí obtenidos podrían ser utilizados en otros análisis, siempre bajo el compromiso de total confidencialidad. Bajo ninguna circunstancia publicaremos ningún dato obtenido bajo alguna forma que permita identificarlo y si son presentados en el marco de una publicación científica, toda información relativa a su identidad será eliminada.
                 </p>
                 <h2>¿Tendrá usted acceso a los datos o resultados de este estudio? </h2>
                 <p>
@@ -63,11 +63,11 @@ const Consentimiento = () => {
                 </p>
                 <h2>¿Es obligación participar? ¿Puede arrepentirse después de participar?</h2>
                 <p>
-                  Usted NO está obligado de ninguna manera a participar en este estudio. Si accede a participar, puede dejar de hacerlo en cualquier momento sin repercusión alguna.
+                  Su participación es completamente voluntaria. Se puede retirar del estudio en el momento que estime conveniente. Para ello, basta con cerrar la tarea antes de enviar los datos al final del estudio.
                 </p>
                 <h2>¿A quién puede contactar Ud. para saber más de este estudio o si le surgen dudas?</h2>
                 <p>
-                  Si tiene cualquier pregunta acerca de esta investigación, puede contactar a la Dra. Constanza Baquedano (E-mail: constanzabaquedano@gmail.com, cel:991240886). Si usted tiene alguna consulta o preocupación respecto a sus derechos como participante de este estudio, puede contactar a Paula Rojas (paula.rojas.s@uai.cl, teléfono +562 2331 1812) directora de investigación de la Universidad Adolfo Ibáñez.
+                  Si usted tiene alguna consulta o preocupación respecto a sus derechos como participante de este estudio, puede contactar a Paola Cañon (paola.canon.g@uai.cl, teléfono +562 2331 1277) directora de investigación de la Universidad Adolfo Ibáñez.
                 </p>
               </div>
             </div>
@@ -77,7 +77,7 @@ const Consentimiento = () => {
             </div>
           </>
         : <div className="Consentimiento__contenedor">
-            <p className="Consentimiento__texto_aprobacion">He tenido la oportunidad de leer esta declaración de consentimiento informado, hacer preguntas acerca del proyecto de investigación, y acepto participar en este proyecto.
+            <p className="Consentimiento__texto_aprobacion">Declaro que he sido informado e invitado a participar en una investigación denominada “Entendiendo la percepción de imágenes de comida en el contexto de una aplicación para smartphone”, este es un proyecto de investigación científica que cuenta con el respaldo de la Universidad Adolfo Ibáñez.
             </p>
             <form
               className="Consentimiento__formulario_aprobar"
